7a9160a36d7357c9a61be07a55360f3246742ec8
Ozzy Tasci <oguzcantasci@gmail.com>
2022-12-14 04:38:07 -0500

    Made it possible to control the snake

--- input.js
+++ input.js
@@ -1,10 +1,28 @@
+let connection;
+
 const handleUserInput = function(data) {
   if (data === '\u0003') {
     process.exit();
   }
+
+  if (data === 'w') {
+    connection.write("Move: up");
+  }
+  if (data === 's') {
+    connection.write("Move: down");
+  }
+  if (data === 'a') {
+    connection.write("Move: left");
+  }
+  if (data === 'd') {
+    connection.write("Move: right");
+  }
+
+
 };
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
