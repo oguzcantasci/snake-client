b445aa79f66f8881f01877706c158e9ad255f87b
Ozzy Tasci <oguzcantasci@gmail.com>
2022-12-17 05:06:51 -0500

    Refactor code with the use of constants

--- input.js
+++ input.js
@@ -1,56 +1,27 @@
+const {moveCommands, messages} = require('./constants');
 let connection;
 let intervalId;
-let lastCommand;
+let lastCommand = true;
 
 // helper function to be used as a callback to handle key inputs;
 const handleUserInput = function(data) {
-  
-  
   if (data === '\u0003') { // exit the game if Ctrl + C is pressed
     process.exit();
   }
   //Moving commands
-  if ((data === 'w' || data === "W") && lastCommand !== 'Move: down') {
-    clearInterval(intervalId);
-    intervalId = setInterval(() => {
-      connection.write("Move: up");
-    }, 50);
-    lastCommand = "Move: up";
+  if (moveCommands[data]) {
+    if (moveCommands[data].cancel !== lastCommand.cancel) {
+      clearInterval(intervalId);
+      lastCommand = moveCommands[data];
+      intervalId = setInterval(() => {
+        connection.write(moveCommands[data].command);
+      }, 50);
+    }
   }
-  if (data === 's' && lastCommand !== "Move: up") {
-    clearInterval(intervalId);
-    intervalId = setInterval(() => {
-      connection.write("Move: down");
-    }, 50);
-    lastCommand = "Move: down";
-  }
-  if (data === 'a' && lastCommand !== "Move: right") {
-    clearInterval(intervalId);
-    intervalId = setInterval(() => {
-      connection.write("Move: left");
-    },50);
-    lastCommand = "Move: left";
-  }
-  if (data === 'd' && lastCommand !== "Move: left") {
-    clearInterval(intervalId);
-
-    intervalId = setInterval(() => {
-      connection.write("Move: right");
-    }, 50);
-    lastCommand = "Move: right";
-  }
+ 
   // Message inputs
-  if (data === 'j') {
-    connection.write("Say: Ez Pz");
-  }
-  if (data === 'i') {
-    connection.write("Say: Can't Beat Me");
-  }
-  if (data === 'k') {
-    connection.write("Say: Nokia Vibez");
-  }
-  if (data === 'l') {
-    connection.write("Say: Must Eat Pixels");
+  if (messages[data]) {
+    connection.write(messages[data]);
   }
 
 
