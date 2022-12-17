1e82b71d61cf253be4bcbe342a13a0794e568022
Ozzy Tasci <oguzcantasci@gmail.com>
2022-12-17 06:00:36 -0500

    Add a promt feature to ask the user for their name & comments for better readability

--- client.js
+++ client.js
@@ -1,5 +1,10 @@
 const net = require("net");
+const prompt = require("prompt-sync")({ sigint: true });
 const { IP, PORT } = require("./constants");
+
+// Prompt to ask the user for their name
+const name = prompt("Enter your name\n");
+console.log(`Name set to ${name}.`);
 
 // establishes a connection with the game server
 const connect = function() {
@@ -7,21 +12,16 @@ const connect = function() {
     host: IP,
     port: PORT,
   });
-  
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
   conn.on("data", async(data) => {
     console.log(data);
   });
-
+  // Log a successfull connection message and send the name to the server
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
-    conn.write("Name: OZY");
+    conn.write(`Name: ${name}`);
   });
-
-
-
   return conn;
 };
 
